93d57df80ff132150c97127f646793dfde349ee0
Teja-m9 <ramtejaramteja9322@gmail.com>
2024-12-29 12:11:46 +0530

    Add the new file

---
+++ Arrays/second_largest.java
@@ -0,0 +1,19 @@
+package Arrays;
+public class second_largest {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,5,10};
+        int max=Integer.MIN_VALUE;
+        int second_largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>second_largest&&arr[i]!=max){
+                second_largest=arr[i];
+            }
+        }
+        System.out.println(second_largest);
+    }
+}
