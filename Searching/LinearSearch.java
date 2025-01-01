787f14210a9bb8534e74917f1756d8a68a30e6aa
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-01-01 15:50:47 +0530

    add the new folder

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,23 @@
+package Searching;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int key = 3;
+        int index = linearSearch(arr, key);
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
