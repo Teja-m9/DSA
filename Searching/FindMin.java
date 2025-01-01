247d6fda83f1a20732c820abf374b88095f2eeb5
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-01-01 21:28:41 +0530

    add the new files

---
+++ Searching/FindMin.java
@@ -0,0 +1,20 @@
+package Searching;
+
+public class FindMin {
+    public static void main(String[] args){
+        int[] arr={1,2,3,95,-100,-3};
+        System.out.println(Search(arr));
+    }
+    public static int Search(int[] arr){
+        if(arr.length==0){
+            return -1;
+        }
+        int min=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        return min;
+    }
+}
