2d7d24bd49c4768c5b9ef4171615bff6e9976a08
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-01-25 23:35:16 +0530

    add the new files

---
+++ Leetcode/Linear81.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+public class Linear81 {
+    public static void main(String[] args){
+    int[] arr={2,5,6,0,0,1,2};
+    int target=0;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]==target){
+            System.out.println("True");
+            return;
+        }
+    }
+    }
+}
