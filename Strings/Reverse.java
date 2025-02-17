5e570be7a873a5d97214394af8dbca3793709133
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-02-17 23:33:32 +0530

    add the new files

---
+++ Strings/Reverse.java
@@ -0,0 +1,10 @@
+package Strings;
+public class Reverse {
+    public static void main(String[] args){
+        String s="Hello";
+        char[] ch=s.toCharArray();
+        for(int i=ch.length-1;i>=0;i--){
+            System.out.print(ch[i]);
+        }
+    }
+}
