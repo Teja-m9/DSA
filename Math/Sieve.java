d7355e5e0710a92f7c3104b8b2afa9cdae021805
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-02-18 14:50:04 +0530

    add the new files

--- Math/Sieve.java
+++ Math/Sieve.java
@@ -7,7 +7,7 @@ public class Sieve {
         sieve(n,primes);
     }
     public static void sieve(int n,boolean[] primes){
-        for(int i=2;i<=n;i++){
+        for(int i=2;i*i<=n;i++){
             if(!primes[i]){
                 for(int j=i*2;j<=n;j+=i){
                     primes[j]=true;
