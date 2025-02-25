a195838bbdb99bc17620eb6597a5183aa7b9f13c
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-02-25 23:19:03 +0530

    add these new files

---
+++ Arrays/Basic.java
@@ -0,0 +1,10 @@
+package Arrays;
+import java.util.*;
+public class Basic {
+    public static void main(String[] args){
+        int[] arr={4,5,3,2,1};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
