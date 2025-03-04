2565e2688f35c7f1315f37edd93ea92b14ac647b
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-03-04 14:57:29 +0530

    add the new files

---
+++ Recursion/Rever.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class Rever {
+    static int sum=0;
+    public static void reverse(int n){
+        if(n==0){
+            return;
+        }
+        int rem=n%10;
+        sum=sum*10+rem;
+        reverse(n/10);
+    }
+    public static void main(String[] args){
+        int n=1234;
+        reverse(n);
+        System.out.println(sum);
+    }
+}
