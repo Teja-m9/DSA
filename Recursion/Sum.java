c91439777379d98c3802123ea26ac9846f676090
Teja-m9 <ramtejaramteja9322@gmail.com>
2025-03-04 14:02:56 +0530

    add the new files

---
+++ Recursion/Sum.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class Sum{
+    public static void main(String[] args){
+        int n=1431;
+        System.out.println(sum(n));
+    }
+    public static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n%10+sum(n/10);
+    }
+}
